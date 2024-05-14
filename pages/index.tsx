9a9ea7dc786fcff6f8a73383fe89d9b224599393
kaisersenjuu <163971591+kaisersenjuu@users.noreply.github.com>
2024-05-14 19:47:32 +0800

    added layout higher order component, added general types file for props and prop-types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,14 @@
+import Layout from "@/hoc/Layout";
 import { NextPage } from "next";
+import { Fragment } from "react";
 
 const Homepage: NextPage = (): React.ReactElement => {
   return (
-    <div className="">
-      <h1>The</h1>
-      <h2>test</h2>
-      <h3>test</h3>
-      <h4>test</h4>
-      <h5>test</h5>
-      <h6>test</h6>
-      <p>test</p>
-      <p>test</p>
-    </div>
+    <Fragment>
+      <Layout>
+        <h1>hello world</h1>
+      </Layout>
+    </Fragment>
   );
 };
 
